e669592ad06d73c1a6cb47e6c31655b168b23f9a
Hyunnin04 <160579920+Hyunnin04@users.noreply.github.com>
2024-10-13 17:00:08 +0700

    Bonus_1.dart

--- W1-S2-PRACTICE-Dart/Bonus_1.dart
+++ W1-S2-PRACTICE-Dart/Bonus_1.dart
@@ -1,4 +1,4 @@
-enum Direction { NORTH, EAST, SOUTH, WEST }
+enum Direction { North, East, South, West}
 class Robot {
   int x;
   int y;
@@ -14,16 +14,16 @@ class Robot {
   }
   void advance() {
     switch (direction) {
-      case Direction.NORTH:
+      case Direction.North:
         y += 1;
         break;
-      case Direction.EAST:
+      case Direction.East:
         x += 1;
         break;
-      case Direction.SOUTH:
+      case Direction.South:
         y -= 1;
         break;
-      case Direction.WEST:
+      case Direction.West:
         x -= 1;
         break;
     }
@@ -49,7 +49,7 @@ class Robot {
   }
 }
 void main() {
-  Robot robot = Robot(7, 3, Direction.NORTH);
+  Robot robot = Robot(7, 3, Direction.North);
   String instructions = "RAALAL";
   robot.Instructions(instructions);
   robot.Position(); 
